af719ebce950086fa07951fc34414227f95d2abb
aashay5 <aashaychaturvedi1@gmail.com>
2021-07-15 08:55:02 +0530

    Initial commit

---
+++ app/src/main/java/com/example/asynctask/ExampleAsyncTask.java
@@ -0,0 +1,18 @@
+package com.example.asynctask;
+
+
+import android.os.AsyncTask;
+import android.os.SystemClock;
+import android.util.Log;
+
+public class ExampleAsyncTask extends AsyncTask<Integer, Void, Void> {
+    private static final String TAG = "ExampleAsyncTask";
+    @Override
+    protected Void doInBackground(Integer... integers) {
+        for (int i=integers[0]; i>0; i--){
+            Log.d(TAG, "doInBackground: i: " + i);
+            SystemClock.sleep(1000);
+        }
+        return null;
+    }
+}
